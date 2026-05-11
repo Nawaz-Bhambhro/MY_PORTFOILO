bf7da4406209a6b1e310da8c0c25730992903d7a
MUHAMMAD NAWAZ <raj@Muhammad-Nawaz-Mac.local>
2026-05-11 07:44:40 +0300

    udpate

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,13 +1,14 @@
 import Hero from "../Section/Hero"
 import About_wraper from "../Section/About_wraper"
-import grid_content from "../Section/grid_content"
+import Work_process from "../Section/Work_process"
+import Portfolio_cards from "../Section/Portfolio_cards"
 
 const Home = () => {
     return (
         <div className="home-wrapper">
             <Hero />
             <About_wraper />
-            <grid_content />
+            <Work_process />
         </div>
     )
 }
