26186caa2a9f3e3b005e001fea21f5a65a517e68
MUHAMMAD NAWAZ <raj@Muhammad-Nawaz-Mac.local>
2026-05-11 07:44:51 +0300

    udpate

---
+++ portfolio/src/Section/Portfolio_cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Style/Portfolio_cards.css'
+const Portfolio_cards = () => {
+  return (
+    <div>
+      <h1>Portfolio Cards</h1>
+    </div>
+  )
+}
+
+export default Portfolio_cards
